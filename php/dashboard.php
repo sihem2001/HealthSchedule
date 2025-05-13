3c9e4eba2746eb3394fbf10d3a68351a6d787f86
hannaziouani <hana.ziouani@gmail.com>
2025-05-13 02:43:03 -0700

    added the page dashboard- like a welcome page , where i may be add another information about the user

---
+++ php/dashboard.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if ( !isset($_SESSION['user_id'])){
+    header('Location: login.php');
+    exit();
+}
+?>
+<!-- html part -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+    <h1> HELLO IN HEALTH-AGENDA</h1>
+    <P>YOU ARE CONNECTE ; YOUR ID <?= htmlspecialchars($_SESSION['user_id']) ?></P>
+      <button><a href="logout.php">LOGOUT</a></button>
+ </body>
+ </html>
